a19360274ae9e0ae78b0ba33906dc4be252f43ca
alv67 <alv67.web@gmail.com>
2024-04-06 00:27:15 +0200

    Parse headers

--- app/main.js
+++ app/main.js
@@ -7,19 +7,38 @@ console.log("Logs from your program will appear here!");
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
     var request = data.toString().split('\r\n');
+    var headers = {};
     var response = '';
     //check for path
     path = request[0].split(' ')[1];
     console.log(`Path: '${path}'`);
+    
+    // parse headers
+    for (var line of request.slice(1)) {
+        // empty line marks end of headers
+        if (line.trim() === '') break;
+
+        // split line into key-value pair
+        var [key, ...value] = line.trim().split(':');
+        headers[key.trim()] = value.join(':').trim() // join(':') to recontruct values wich have : inside
+    }
+    console.log(`headers:\n${JSON.stringify(headers)}`);
+
     if (path === '/') {
       response  = "HTTP/1.1 200 OK\r\n\r\n";
     } else if (path.trim().startsWith('/echo/')) {
-        let echoContent = path.trim().substring('/echo/'.length);
-        console.log(`echo: ${echoContent}`);
+        var body = path.trim().substring('/echo/'.length);
+        console.log(`echo: ${body}`);
         var response = `HTTP/1.1 200 OK\r\n`;
         response += `Content-Type: text/plain\r\n`;
-        response += `Content-Length: ${echoContent.length}\r\n`;
-        response += `\r\n${echoContent}\r\n`;
+        response += `Content-Length: ${body.length}\r\n`;
+        response += `\r\n${body}\r\n`;
+    } else if (path.trim().startsWith('/user-agent')){
+        var body = headers["User-Agent"];
+        var response = `HTTP/1.1 200 OK\r\n`;
+        response += `Content-Type: text/plain\r\n`;
+        response += `Content-Length: ${body.length}\r\n`;
+        response += `\r\n${body}\r\n`;
     } else {        
       response = "HTTP/1.1 404 Not Found\r\n\r\n";
     }
