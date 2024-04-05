ff5fa61e44d78a484a0155b7d1e1f53482bec0cf
alv67 <alv67.web@gmail.com>
2024-04-05 12:12:13 +0000

    Respond with 404

--- app/main.js
+++ app/main.js
@@ -5,8 +5,16 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-  socket.on("data", () => {
-    socket.write("HTTP/1.1 200 OK\r\n\r\n");
+  socket.on("data", (data) => {
+    var message = data.toString().split('\r\n');
+    //check for path
+    path = message[0].split(' ')[1];
+    console.log(`Path: '${path}'`);
+    if (path === '/') {
+      socket.write("HTTP/1.1 200 OK\r\n\r\n");
+    } else {
+      socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+    }
     socket.end();
   });
   socket.on("close", () => {
