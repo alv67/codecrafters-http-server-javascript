55826262c452d60ca925b532384d348ef918b119
alv67 <alv67.web@gmail.com>
2024-04-05 23:58:44 +0200

    REspond with content

--- app/main.js
+++ app/main.js
@@ -6,15 +6,24 @@ console.log("Logs from your program will appear here!");
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
-    var message = data.toString().split('\r\n');
+    var request = data.toString().split('\r\n');
+    var response = '';
     //check for path
-    path = message[0].split(' ')[1];
+    path = request[0].split(' ')[1];
     console.log(`Path: '${path}'`);
     if (path === '/') {
-      socket.write("HTTP/1.1 200 OK\r\n\r\n");
-    } else {
-      socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+      response  = "HTTP/1.1 200 OK\r\n\r\n";
+    } else if (path.trim().startsWith('/echo/')) {
+        let echoContent = path.trim().substring('/echo/'.length);
+        console.log(`echo: ${echoContent}`);
+        var response = `HTTP/1.1 200 OK\r\n`;
+        response += `Content-Type: text/plain\r\n`;
+        response += `Content-Length: ${echoContent.length}\r\n`;
+        response += `\r\n${echoContent}\r\n`;
+    } else {        
+      response = "HTTP/1.1 404 Not Found\r\n\r\n";
     }
+    socket.write(response);
     socket.end();
   });
   socket.on("close", () => {
